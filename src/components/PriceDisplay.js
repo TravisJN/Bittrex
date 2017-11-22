68c724bf44c2e88e51aa6f77d15f01609763031a
travisjn <tdizzletrav@gmail.com>
2017-11-21 18:08:53 -0800

    Created PriceModel

--- src/components/PriceDisplay.js
+++ src/components/PriceDisplay.js
@@ -1,40 +1,10 @@
 import React from 'react';
 import '../App.js';
 import TextInputForm from './TextInputForm.js';
+import PriceModel from '../data/PriceModel.js';
 //import KEYS from '../private/Keys.js'
 
 export class PriceDisplay extends React.Component{
-    endPoints = {
-        getMarkets: {
-            path: '/public/getmarkets'
-        },
-        getCurrencies: {
-            path: '/public/getcurrencies'
-        },
-        getTicker: {
-            path: '/public/getticker',
-            queryParams: {
-                market: '' // (required) a string literal for the market (ex: BTC-LTC)
-            }
-        },
-        getMarketSummaries: {
-            path: '/public/getmarketsummaries'
-        },
-        getMarketSummary: {
-            path: '/public/getmarketsummary',
-            queryParams: {
-                market: '' // (required) a string literal for the market (ex: BTC-LTC)
-            }
-        },
-        getOrderBook: {
-            path: '/public/getorderbook',
-            queryParams: {
-                market: '', // (required) a string literal for the market (ex: BTC-LTC)
-                type: '',   // (required) buy, sell or both to identify the type of orderbook to return
-                depth: ''   // (optional) defaults to 20 - how deep of an order book to retrieve. Max is 50
-            }
-        }
-    }
 
     constructor(props) {
         super(props);
@@ -44,12 +14,19 @@ export class PriceDisplay extends React.Component{
             price: 0,
             onSubmit: this.onSubmit.bind(this)
         }
+
+        this.mModel = new PriceModel();
     }
 
     render() {
-        var balances = this.state.balances.map((aCurrency) => {
-            return <li>{aCurrency.Currency} : {aCurrency.Balance}</li>
-        })
+        var balances = this.state.balances
+                        .filter((aCurrency) => {
+                            return aCurrency.Balance > 0;
+                        })
+                        .map((aCurrency) => {
+                            return <li key={aCurrency.Currency}>{aCurrency.Currency} : {aCurrency.Balance}</li>
+                        });
+
         return( 
         <div>
             <TextInputForm onSubmit={this.state.onSubmit}/>
@@ -60,31 +37,11 @@ export class PriceDisplay extends React.Component{
 
     onSubmit(event) {
         var tickerSymbol = event.target.value;
-        // dispatch an action here
-        this.fetchPrice().then((value) => {
-            this.setState({balances: value.result});
-            console.log(value);
-        })
-    }
 
-    fetchPrice() {
-        //var path = '/public/getmarkets';
-        var path = '/account/getbalances',
-            qp = '?apikey=4973ea1118f14c5eb9699bc4cf5f1c3a',
-            url = ['http://localhost:8080', path, qp].join('');
-         return fetch(url, {  
-            method: 'GET',
-            headers: {
-                'Access-Control-Request-Method': 'GET',
-            }
+        this.mModel.fetchPrice().then((value) => {
+            this.setState({balances: value.result});
+            console.log(value.result);
         })
-        .then((response) => {
-             return response.json();
-        })
-        .catch((error) => {
-            console.log('error', error);
-            this.setState({price: 'ERROR'});
-        });
     }
 }
 
--- src/data/PriceModel.js
+++ src/data/PriceModel.js
@@ -1,5 +1,58 @@
 export class PriceModel {
-    
+
+    constructor() {
+
+    }
+
+    endPoints = {
+        getMarkets: {
+            path: '/public/getmarkets'
+        },
+        getCurrencies: {
+            path: '/public/getcurrencies'
+        },
+        getTicker: {
+            path: '/public/getticker',
+            queryParams: {
+                market: '' // (required) a string literal for the market (ex: BTC-LTC)
+            }
+        },
+        getMarketSummaries: {
+            path: '/public/getmarketsummaries'
+        },
+        getMarketSummary: {
+            path: '/public/getmarketsummary',
+            queryParams: {
+                market: '' // (required) a string literal for the market (ex: BTC-LTC)
+            }
+        },
+        getOrderBook: {
+            path: '/public/getorderbook',
+            queryParams: {
+                market: '', // (required) a string literal for the market (ex: BTC-LTC)
+                type: '',   // (required) buy, sell or both to identify the type of orderbook to return
+                depth: ''   // (optional) defaults to 20 - how deep of an order book to retrieve. Max is 50
+            }
+        }
+    }
+
+    fetchPrice() {
+        var path = '/account/getbalances',
+        url = ['http://localhost:8080', path].join('');
+        return fetch(url, {  
+            method: 'GET',
+            headers: {
+                'Access-Control-Request-Method': 'GET',
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.log('error', error);
+            this.setState({price: 'ERROR'});
+        });
+    }
 }
 
 export default PriceModel;
